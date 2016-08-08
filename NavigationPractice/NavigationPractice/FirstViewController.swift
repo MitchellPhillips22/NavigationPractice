da1230f76ed82608615959ebeac6cda409da5237
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-08-07 20:43:54 -0600

    Initial commit of project

---
+++ NavigationPractice/NavigationPractice/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NavigationPractice
+//
+//  Created by Mitchell Phillips on 8/7/16.
+//  Copyright © 2016 MitchellPhillips. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
